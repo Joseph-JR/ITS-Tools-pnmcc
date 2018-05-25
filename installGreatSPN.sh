8b81673fafbabaf177c31e782a047871431d038c
Joseph-JR <34281346+Joseph-JR@users.noreply.github.com>
2018-05-25 15:27:13 +0200

    update

--- installGreatSPN.sh
+++ installGreatSPN.sh
@@ -1,12 +1,9 @@
 #!/bin/bash
 
 
-var="https://thamazghasmail.github.io/ITS-commandline/fr.lip6.move.gal.itscl.product-linux.gtk.x86_64.zip"
-archive="fr.lip6.move.gal.itscl.product-linux.gtk.x86_64.zip"
-wget $var
-unzip $archive -d destination
+
 var1="https://github.com/ThamazghaSMAIL/GSPN/raw/master/GSPN.zip"
 archive1="GSPN.zip"
 wget $var1
-unzip $archive1 -d destination
+unzip $archive1 -d .
 echo "fin de l'installation"
